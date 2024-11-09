2e429bd3b493a05f5465738da63f0f5ed628283a
Jerald Calunsag <htcteambangis@gmail.com>
2024-11-09 17:56:49 +0800

    Initial commit

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -27,7 +27,10 @@ app.use(express.json());
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+// Task 2: Add the secondChanceItemsRoutes to the server by using the app.use() method
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
+
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
