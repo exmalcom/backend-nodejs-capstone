e22017bed04db613190a0b0dd4b08f880ef8174e
Jerald Calunsag <htcteambangis@gmail.com>
2024-11-09 18:57:25 +0800

    Create API for Search Service Done

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -24,52 +24,14 @@ const upload = multer({ storage: storage });
 
 // Get all secondChanceItems
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
     try {
-        //Step 2: Task 1: Retrieve the database connection from db.js
         const db = await connectToDatabase();
-        //Step 2: Task 2: Use the collection() method to retrieve the secondChanceItems collection
+
         const collection = db.collection("secondChanceItems");
-        //Step 2: Task 3: Fetch all secondChanceItems using collection.find().toArray()
         const secondChanceItems = await collection.find({}).toArray();
-        //Step 2: Task 4: Return the secondChanceItems using res.json()
         res.json(secondChanceItems);
     } catch (e) {
-        logger.console.error('oops something went wrong', e)
-        next(e);
-    }
-});
-
-// Add a new item
-router.post('/', upload.single('file'), async(req, res,next) => {
-    try {
-
-        //Step 3: Task 1: Retrieve the database connection from db.js
-        const db = await connectToDatabase();
-        //Step 3: Task 2: Use the collection() method to retrieve the secondChanceItems collection
-        const collection = db.collection("secondChanceItems");
-        //Step 3: Task 3: Create a new secondChanceItem from the request body
-        let secondChanceItem = req.body;
-        //Step 3: Task 4: Get the last id, increment it by 1, and set it to the new secondChanceItem
-        const lastItemQuery = await collection.find().sort({'id': -1}).limit(1);
-        await lastItemQuery.forEach(item => {
-            secondChanceItem.id = (parseInt(item.id) + 1).toString();
-        });
-        //Step 3: Task 5: Set the current date in the new item
-        const date_added = Math.floor(new Date().getTime() / 1000);
-        secondChanceItem.date_added = date_added;
-        // Task 6: Add the secondChanceItem to the database
-        secondChanceItem = await collection.insertOne(secondChanceItem);
-        // Task 7: Upload the image to the images directory (handled by multer automatically)
-
-        // Check if file was uploaded
-        if (req.file) {
-            secondChanceItem.filePath = `/images/${req.file.filename}`; // Store the file path for later use
-        }
-        // Send the newly created item back as the response
-        res.status(201).json(secondChanceItem.ops[0]); // Return the inserted item
-    } catch (e) {
-        logger.console.error('Error adding secondChanceItem', e);
+        logger.console.error('Something went wrong ', e)
         next(e);
     }
 });
@@ -77,21 +39,40 @@ router.post('/', upload.single('file'), async(req, res,next) => {
 // Get a single secondChanceItem by ID
 router.get('/:id', async (req, res, next) => {
     try {
-        //Step 4: // Task 1: Retrieve the database connection from db.js
         const db = await connectToDatabase();
-        //Step 4: Task 2: Use the collection() method to retrieve the secondChanceItems collection
         const collection = db.collection("secondChanceItems");
-        //Step 4: Task 3: Find a specific secondChanceItem by its ID
-        const id = req.params.id; // Get the ID from the request parameters
+        const id = req.params.id;
         const secondChanceItem = await collection.findOne({ id: id });
-        //Step 4: Task 4: Return the secondChanceItem as a JSON object or return an error message if not found
+
         if (!secondChanceItem) {
             return res.status(404).send("secondChanceItem not found");
         }
-        // Return the found item as a JSON response
+
         res.json(secondChanceItem);
     } catch (e) {
-        logger.console.error('Error fetching secondChanceItem by ID', e);
+        next(e);
+    }
+});
+
+
+// Add a new item
+router.post('/', upload.single('file'), async(req, res,next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("secondChanceItems");
+        const lastItemQuery = await collection.find().sort({'id': -1}).limit(1);
+        let secondChanceItem = req.body;
+
+        await lastItemQuery.forEach(item => {
+            secondChanceItem.id = (parseInt(item.id) + 1).toString();
+        });
+        const date_added = Math.floor(new Date().getTime() / 1000);
+        secondChanceItem.date_added = date_added
+
+        secondChanceItem = await collection.insertOne(secondChanceItem);
+        console.log(secondChanceItem);
+        res.status(201).json(secondChanceItem);
+    } catch (e) {
         next(e);
     }
 });
@@ -99,45 +80,37 @@ router.get('/:id', async (req, res, next) => {
 // Update and existing item
 router.put('/:id', async(req, res,next) => {
     try {
-        //Step 5: Task 1: Retrieve the database connection from db.js
         const db = await connectToDatabase();
-        //Step 5: Task 2: Use the collection() method to retrieve the secondChanceItems collection
         const collection = db.collection("secondChanceItems");
+        const id = req.params.id;
+        const secondChanceItem = await collection.findOne({ id });
 
-        const id = req.params.id; // Get the ID from the request parameters
-
-        //Step 5: Task 3: Check if the secondChanceItem exists
-        const secondChanceItem = await collection.findOne({ id: id });
         if (!secondChanceItem) {
             logger.error('secondChanceItem not found');
             return res.status(404).json({ error: "secondChanceItem not found" });
         }
-        //Step 5: Task 4: Update the item's attributes
-        secondChanceItem.category = req.body.category || secondChanceItem.category;
-        secondChanceItem.condition = req.body.condition || secondChanceItem.condition;
-        secondChanceItem.age_days = req.body.age_days || secondChanceItem.age_days;
-        secondChanceItem.description = req.body.description || secondChanceItem.description;
 
-        // Calculate age_years based on age_days (rounded to one decimal place)
-        secondChanceItem.age_years = Number((secondChanceItem.age_days / 365).toFixed(1));
-
-        // Set the updatedAt field to the current date
+        secondChanceItem.category = req.body.category;
+        secondChanceItem.condition = req.body.condition;
+        secondChanceItem.age_days = req.body.age_days;
+        secondChanceItem.description = req.body.description;
+        secondChanceItem.age_years = Number((secondChanceItem.age_days/365).toFixed(1));
         secondChanceItem.updatedAt = new Date();
 
-        // Update the item in the database
-        const updateResult = await collection.findOneAndUpdate(
-            { id: id },
+        const updatepreloveItem = await collection.findOneAndUpdate(
+            { id },
             { $set: secondChanceItem },
             { returnDocument: 'after' }
         );
-        //Step 5: Task 5: Send confirmation
-        if (updateResult.value) {
-            res.json({ "uploaded": "success" });
+
+
+        if(updatepreloveItem) {
+            res.json({"uploaded":"success"});
         } else {
-            res.json({ "uploaded": "failed" });
+            res.json({"uploaded":"failed"});
         }
+
     } catch (e) {
-        logger.console.error('Error updating secondChanceItem', e);
         next(e);
     }
 });
@@ -145,25 +118,19 @@ router.put('/:id', async(req, res,next) => {
 // Delete an existing item
 router.delete('/:id', async(req, res,next) => {
     try {
-        //Step 6: Task 1: Retrieve the database connection from db.js
         const db = await connectToDatabase();
-        //Step 6: Task 2: Use the collection() method to retrieve the secondChanceItems collection
         const collection = db.collection("secondChanceItems");
+        const id = req.params.id;
+        const secondChanceItem = await collection.findOne({ id });
 
-        const id = req.params.id; // Get the ID from the request parameters
-        //Step 6: Task 3: Find the secondChanceItem by ID
-        const secondChanceItem = await collection.findOne({ id: id });
         if (!secondChanceItem) {
             logger.error('secondChanceItem not found');
             return res.status(404).json({ error: "secondChanceItem not found" });
         }
-        //Step 6: Task 4: Delete the secondChanceItem
-        await collection.deleteOne({ id: id });
+        const updatepreloveItem = await collection.deleteOne({ id });
 
-        // Send a success response after deletion
-        res.json({ "deleted": "success" });
+        res.json({"deleted":"success"});
     } catch (e) {
-        logger.error('Error deleting secondChanceItem', e);
         next(e);
     }
 });
